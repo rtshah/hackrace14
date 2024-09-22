604bbd4641838fa79e26f9136a8f90cf90905e72
4Gleneyre <33353768+4Gleneyre@users.noreply.github.com>
2024-09-22 03:17:21 -0500

    .

--- hackrace14/tailwind.config.js
+++ hackrace14/tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [],
 }
